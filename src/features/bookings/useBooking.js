4c51aae51255913a48da78280c8873ce922b2206
SusieWen777 <wen_bufan11@163.com>
2024-02-15 17:41:22 +1100

    add menu and detail button to booking row

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+  // To get the id from the URL
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(bookingId),
+    // by default, React query will fetch data three times. But in this case, could find data means doesn't exist and no need to fetch again
+    retry: false,
+  });
+
+  return { isLoading, error, booking };
+}
