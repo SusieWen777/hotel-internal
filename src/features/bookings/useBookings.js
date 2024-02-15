1019f6aa5a04a91f077b20cc1cced0e866f2a51c
SusieWen777 <wen_bufan11@163.com>
2024-02-15 14:30:48 +1100

    add pre-fetching for pagination in booking

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
   // Get value from the URL
   const [searchParams] = useSearchParams();
+
+  // For pre-fetching data
+  const queryClient = useQueryClient();
 
   //FILTER
   const filterValue = searchParams.get("status");
@@ -19,18 +23,37 @@ export function useBookings() {
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
-  //Pagination
+  //PAGINATION
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
-  //The second and third parameters in the queryKey acts as a dependency array, which means when the value changes, the query should refetch the data
+  //QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
     error,
   } = useQuery({
+    //The last three parameters in the queryKey acts as a dependency array, which means when the value changes, the query should refetch the data
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  //PRE-FETCHING (both backward and forward)
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  // only do pre-fetching when not at the last page
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  // only do pre-fetching when not at the first page
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { isLoading, error, bookings, count };
 }
